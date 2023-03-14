8cee81cdbf59c79a7942473cdaaa46bc1e7ac4fe
Konrad Droeske <kdroeske@gmail.com>
2023-03-14 14:18:16 -0400

    valid palindrome

---
+++ src/test/kotlin/leetcode/twoPointers/ValidPalindromeTest.kt
@@ -0,0 +1,30 @@
+package leetcode.twoPointers
+
+import ValidPalindrome
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class ValidPalindromeTest {
+    val solution = ValidPalindrome()
+
+    @Test
+    fun isPalindrome1() {
+        val input = "A man, a plan, a canal: Panama"
+        val result = solution.isPalindromeTwoPointer(input)
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun isPalindrome2() {
+        val input = "race a car"
+        val result = solution.isPalindromeTwoPointer(input)
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun isPalindrome3() {
+        val input = " "
+        val result = solution.isPalindromeTwoPointer(input)
+        assertEquals(true, result)
+    }
+}
