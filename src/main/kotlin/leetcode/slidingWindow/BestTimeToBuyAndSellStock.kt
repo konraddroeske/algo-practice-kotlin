5cfb0bceeed288ae3ebfeffabda06b969a60ebf2
Konrad Droeske <kdroeske@gmail.com>
2023-03-21 12:12:44 -0400

    best time to buy and sell stock

--- src/main/kotlin/leetcode/slidingWindow/BestTimeToBuyAndSellStock.kt
+++ src/main/kotlin/leetcode/slidingWindow/BestTimeToBuyAndSellStock.kt
@@ -2,6 +2,32 @@ package leetcode.slidingWindow
 
 class BestTimeToBuyAndSellStock {
     fun maxProfit(prices: IntArray): Int {
-        TODO()
+        var minVal = Int.MAX_VALUE
+        var profit = 0
+
+        for (price in prices) {
+            minVal = minOf(minVal, price)
+            profit = maxOf(profit, price - minVal)
+        }
+
+        return profit
+    }
+
+    fun maxProfitTwoPointers(prices: IntArray): Int {
+        var left = 0
+        var right = 1
+        var result = 0
+
+        while (right < prices.size) {
+            if (prices[left] < prices[right]) {
+                result = maxOf(result, prices[right] - prices[left])
+            } else {
+                left = right
+            }
+
+            right += 1
+        }
+
+        return result
     }
 }
--- src/test/kotlin/leetcode/slidingWindow/BestTimeToBuyAndSellStockTest.kt
+++ src/test/kotlin/leetcode/slidingWindow/BestTimeToBuyAndSellStockTest.kt
@@ -9,8 +9,16 @@ class BestTimeToBuyAndSellStockTest {
     @Test
     fun maxProfit1() {
         val input = intArrayOf(7, 1, 5, 3, 6, 4)
-        val result = solution.maxProfit(input)
+        val result = solution.maxProfitTwoPointers(input)
 
         assertEquals(5, result)
+    }
+
+    @Test
+    fun maxProfit2() {
+        val input = intArrayOf(7, 6, 4, 3, 1)
+        val result = solution.maxProfitTwoPointers(input)
+
+        assertEquals(0, result)
     }
 }
