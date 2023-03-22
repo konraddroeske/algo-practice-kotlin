4edb4c6c20d3eb481e91d8b1a33deb89427caa43
Konrad Droeske <kdroeske@gmail.com>
2023-03-22 15:18:04 -0400

    use maxFreq for longest repeating character replacement

--- src/main/kotlin/leetcode/slidingWindow/LongestRepeatingCharacterReplacement.kt
+++ src/main/kotlin/leetcode/slidingWindow/LongestRepeatingCharacterReplacement.kt
@@ -4,27 +4,26 @@ class LongestRepeatingCharacterReplacement {
     private fun isOverMax(
         right: Int,
         left: Int,
-        counter: IntArray,
+        maxFreq: Int,
         k: Int,
-    ):
-        Boolean {
-        if (counter.isEmpty()) return false
+    ): Boolean {
         val windowSize = right - left + 1
-        val maxQty = counter.max()
-
-        return windowSize - maxQty > k
+        return windowSize - maxFreq > k
     }
+
     fun characterReplacement(s: String, k: Int): Int {
         val counter = IntArray(26)
         var result = 0
         var leftIndex = 0
+        var maxFreq = 0
 
         for ((rightIndex, curChar) in s.withIndex()) {
-            counter[curChar.code - 65] += 1
+            counter[curChar.toInt() - 65] += 1
+            maxFreq = maxOf(maxFreq, counter[curChar.toInt() - 65])
 
-            while (isOverMax(rightIndex, leftIndex, counter, k)) {
+            while (isOverMax(rightIndex, leftIndex, maxFreq, k)) {
                 val leftChar = s[leftIndex]
-                counter[leftChar.code - 65] -= 1
+                counter[leftChar.toInt() - 65] -= 1
                 leftIndex += 1
             }
 
