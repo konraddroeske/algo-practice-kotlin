eec38b896e35eb017dff666733889d9ac3a943a8
Konrad Droeske <kdroeske@gmail.com>
2023-04-18 16:39:08 -0400

    max area of island

---
+++ src/main/kotlin/leetcode/graph/MaxAreaOfIsland.kt
@@ -0,0 +1,32 @@
+package leetcode.graph
+
+class MaxAreaOfIsland {
+    fun getMaxAreaOfIsland(grid: Array<IntArray>): Int {
+        val rows = grid.size
+        val cols = grid[0].size
+        var maxArea = 0
+
+        for (row in 0 until rows) {
+            for (col in 0 until cols) {
+                maxArea = maxOf(maxArea, dfs(row, col, grid, rows, cols))
+            }
+        }
+
+        return maxArea
+    }
+
+    fun dfs(row: Int, col: Int, grid: Array<IntArray>, rows: Int, cols: Int): Int {
+        val isOutOfBounds = row < 0 || row >= rows || col < 0 || col >= cols
+
+        if (isOutOfBounds || grid[row][col] == 0) {
+            return 0
+        }
+
+        grid[row][col] = 0
+
+        return 1 + dfs(row + 1, col, grid, rows, cols) +
+            dfs(row - 1, col, grid, rows, cols) +
+            dfs(row, col + 1, grid, rows, cols) +
+            dfs(row, col - 1, grid, rows, cols)
+    }
+}
---
+++ src/test/kotlin/leetcode/graph/MaxAreaOfIslandTest.kt
@@ -0,0 +1,25 @@
+package leetcode.graph
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class MaxAreaOfIslandTest {
+    val solution = MaxAreaOfIsland()
+
+    @Test
+    fun getMaxAreaOfIsland() {
+        val grid = arrayOf(
+            intArrayOf(0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0),
+            intArrayOf(0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0),
+            intArrayOf(0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0),
+        )
+
+        val result = solution.getMaxAreaOfIsland(grid)
+        assertEquals(6, result)
+    }
+}
