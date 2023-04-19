c45b9bef6a397e2fec15fdd78b8f6beb2ebb1368
Konrad Droeske <kdroeske@gmail.com>
2023-04-19 17:01:29 -0400

    rotting oranges

--- src/main/kotlin/leetcode/graph/RottingOranges.kt
+++ src/main/kotlin/leetcode/graph/RottingOranges.kt
@@ -1,92 +1,77 @@
 package leetcode.graph
 
-import java.util.*
+// Time - O (rows * cols)
+class RottingOranges {
+    fun orangesRotting(grid: Array<IntArray>): Int {
+        var totalTime = 0
+        val queue = ArrayDeque<Pair<Int, Int>>()
+        var freshOranges = 0
 
-fun orangesRotting(grid: Array<IntArray>): Int {
-    if (grid.isEmpty()) {
-        return -1
+        val rows = grid.size
+        val cols = grid[0].size
+
+        for (row in 0 until rows) {
+            for (col in 0 until cols) {
+                val cur = grid[row][col]
+
+                if (cur == 2) {
+                    queue.add(Pair(row, col))
+                }
+
+                if (cur == 1) {
+                    freshOranges += 1
+                }
+            }
+        }
+
+        val directions = listOf(
+            Pair(1, 0),
+            Pair(-1, 0),
+            Pair(0, 1),
+            Pair(0, -1),
+        )
+
+        while (queue.isNotEmpty() && freshOranges > 0) {
+            repeat(queue.size) {
+                val (curRow, curCol) = queue.removeFirst()
+
+                for ((newRow, newCol) in directions) {
+                    val orange = getFreshOrange(
+                        curRow + newRow,
+                        curCol + newCol,
+                        grid,
+                        rows,
+                        cols,
+                    )
+
+                    if (orange != null) {
+                        freshOranges -= 1
+                        grid[orange.first][orange.second] = 2
+                        queue.add(orange)
+                    }
+                }
+            }
+
+            totalTime += 1
+        }
+
+        return if (freshOranges == 0) totalTime else -1
     }
 
-    val height = grid.size
-    val width = grid[0].size
+    fun getFreshOrange(
+        row: Int,
+        col: Int,
+        grid: Array<IntArray>,
+        rows: Int,
+        cols: Int,
+    ): Pair<Int, Int>? {
+        val isOutOfBounds = row < 0 || row >= rows || col < 0 || col >= cols
 
-    val bfsQueue = LinkedList<Triple<Int, Int, Int>>()
-    val visited = mutableSetOf<Pair<Int, Int>>()
-    var totalOranges = 0
-    var maxDistance = 0
-
-    for ((rowIndex, row) in grid.withIndex()) {
-        for ((colIndex, item) in row.withIndex()) {
-            if (item == 2) {
-                bfsQueue.add(Triple(rowIndex, colIndex, 0))
-                visited.add(Pair(rowIndex, colIndex))
-            }
-
-            if (item == 1 || item == 2) {
-                totalOranges += 1
-            }
+        if (isOutOfBounds || grid[row][col] != 1
+        ) {
+            return null
         }
+
+        return Pair(row, col)
     }
-
-    while (bfsQueue.isNotEmpty()) {
-        val (rowIndex, colIndex, distance) = bfsQueue.remove()
-        val newDistance = distance + 1
-        maxDistance = maxOf(maxDistance, distance)
-
-        if (rowIndex > 0) {
-            val up = grid[rowIndex - 1][colIndex]
-
-            if (up == 1 && !visited.contains(Pair(rowIndex - 1, colIndex))) {
-                bfsQueue.add(Triple(rowIndex - 1, colIndex, newDistance))
-                visited.add(Pair(rowIndex - 1, colIndex))
-            }
-        }
-
-        if (rowIndex < height - 1) {
-            val down = grid[rowIndex + 1][colIndex]
-
-            if (down == 1 && !visited.contains(Pair(rowIndex + 1, colIndex))) {
-                bfsQueue.add(Triple(rowIndex + 1, colIndex, newDistance))
-                visited.add(Pair(rowIndex + 1, colIndex))
-            }
-        }
-
-        if (colIndex > 0) {
-            val left = grid[rowIndex][colIndex - 1]
-
-            if (left == 1 && !visited.contains(Pair(rowIndex, colIndex - 1))) {
-                bfsQueue.add(Triple(rowIndex, colIndex - 1, newDistance))
-                visited.add(Pair(rowIndex, colIndex - 1))
-            }
-        }
-
-        if (colIndex < width - 1) {
-            val right = grid[rowIndex][colIndex + 1]
-
-            if (right == 1 && !visited.contains(Pair(rowIndex, colIndex + 1))) {
-                bfsQueue.add(Triple(rowIndex, colIndex + 1, newDistance))
-                visited.add(Pair(rowIndex, colIndex + 1))
-            }
-        }
-    }
-
-    if (visited.size != totalOranges) {
-        return -1
-    }
-
-    return maxDistance
-}
-
-fun main() {
-    val grid1 = arrayOf(
-        intArrayOf(2, 1, 1),
-        intArrayOf(1, 1, 0),
-        intArrayOf(0, 1, 1),
-    )
-    print(orangesRotting(grid1))
-
-    val grid2 = arrayOf(
-        intArrayOf(0, 2),
-    )
-    print(orangesRotting(grid2))
 }
---
+++ src/test/kotlin/leetcode/graph/RottingOrangesKtTest.kt
@@ -0,0 +1,62 @@
+package leetcode.graph
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class RottingOrangesKtTest {
+    val solution = RottingOranges()
+
+    @Test
+    fun orangesRotting1() {
+        val grid1 = arrayOf(
+            intArrayOf(2, 1, 1),
+            intArrayOf(1, 1, 0),
+            intArrayOf(0, 1, 1),
+        )
+
+        val result = solution.orangesRotting(grid1)
+        assertEquals(4, result)
+    }
+
+    @Test
+    fun orangesRotting2() {
+        val grid1 = arrayOf(
+            intArrayOf(2, 1, 1),
+            intArrayOf(0, 1, 1),
+            intArrayOf(1, 0, 1),
+        )
+
+        val result = solution.orangesRotting(grid1)
+        assertEquals(-1, result)
+    }
+
+    @Test
+    fun orangesRotting3() {
+        val grid2 = arrayOf(
+            intArrayOf(0, 2),
+        )
+
+        val result = solution.orangesRotting(grid2)
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun orangesRotting4() {
+        val grid2 = arrayOf(
+            intArrayOf(1),
+        )
+
+        val result = solution.orangesRotting(grid2)
+        assertEquals(-1, result)
+    }
+
+    @Test
+    fun orangesRotting5() {
+        val grid2 = arrayOf(
+            intArrayOf(1, 2),
+        )
+
+        val result = solution.orangesRotting(grid2)
+        assertEquals(1, result)
+    }
+}
